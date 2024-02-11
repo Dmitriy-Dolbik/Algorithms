dd875b6e9e8015b2e06300ef1e52e3ca15256afd
ddolbik <d.dolbik@crystals.ru>
2024-02-11 22:03:01 +0300

    Refactor BracketsGenerator

--- src/main/java/org/example/YandexContest/BracketsGenerator.java
+++ src/main/java/org/example/YandexContest/BracketsGenerator.java
@@ -8,14 +8,15 @@ import java.io.IOException;
 import java.util.Arrays;
 
 /**
+ * n = 3
  * ((()))
- * )(
+ *   )(
  * (()())
- * )(
+ *    )(
  * (())()
- * )(()
+ *  )((
  * ()(())
- * )(
+ *    )(
  * ()()()
  * ..................
  * [(, (, (, ), ), )]
@@ -30,6 +31,7 @@ public class BracketsGenerator {
     private static BufferedReader bufferedReader;
     private static BufferedWriter bufferedWriter;
     private static final int MAX_ARRAY_SIZE = 15;
+    private static int countOfOpenedBrackets = 0;
 
 
     public static void main(String[] args) throws IOException {
@@ -49,13 +51,17 @@ public class BracketsGenerator {
     }
 
     private static void run() throws IOException {
-        int inputValue = Integer.parseInt(String.valueOf(readLine()).trim());
-        int bracketsNumber = inputValue * 2; // количество скобок, должно быть четное
+        int halfBracketsNumber = Integer.parseInt(String.valueOf(readLine()).trim());
+        int bracketsNumber = halfBracketsNumber * 2;
+        char[] bracketsArray = createTheSmallestBracketSequence(bracketsNumber);
+        createRemainingBracketSequence(bracketsNumber, bracketsArray);
+    }
+
+    private static char[] createTheSmallestBracketSequence(int bracketsNumber) {
         char[] bracketsArray = new char[bracketsNumber];
         Arrays.fill(bracketsArray, 0, bracketsNumber, '(');
         Arrays.fill(bracketsArray, bracketsNumber / 2, bracketsNumber, ')');
-
-        replaceBrackets(bracketsNumber, bracketsArray);
+        return bracketsArray;
     }
 
     private static char[] readLine() throws IOException {
@@ -71,48 +77,55 @@ public class BracketsGenerator {
             readCharArray[i] = (char) readCharacter;
         }
         return readCharArray;
-
     }
 
-    public static void replaceBrackets(int bracketsNumber, char[] bracketsArray) throws IOException {
+    public static void createRemainingBracketSequence(int bracketsNumber, char[] bracketsArray) throws IOException {
         // печатаем нулевую последовательность
         writeLine(bracketsArray);
         while (true) {
-            int bracketIndex = bracketsNumber - 1;
-            int openedBrackets = 0;
-            int closedBrackets = 0;
             // находим откр. скобку, которую можно заменить
-            while (bracketIndex >= 0) { //ищем с последнего элемента
-                if (bracketsArray[bracketIndex] == ')') {
-                    closedBrackets++;
-                }
-                if (bracketsArray[bracketIndex] == '(') {
-                    openedBrackets++;
-                }
-                if (openedBrackets - closedBrackets < 0 && openedBrackets > 0 && bracketsArray[bracketIndex] == '(') {//останавливаем поиск, если закрывающих скобор
-                    // больше, чем открывабщих
-                    break;
-                }
-                bracketIndex--;
-            }
+            int indexOfOpenedBracketsToChange = findIndexOfOpenedBracketToChange(bracketsNumber, bracketsArray);
             // если не нашли, то алгоритм заканчивает работу
-            if (bracketIndex < 0) {
+            if (indexOfOpenedBracketsToChange < 0) {
                 break;
             }
             // заменяем на закр. скобку
-            bracketsArray[bracketIndex] = ')';
-            // заменяем на самую лексикографическую минимальную
-            for (int i = bracketIndex + 1; i < bracketsNumber; i++) {
-                if (openedBrackets > 0) {
-                    bracketsArray[i] = '(';
-                    openedBrackets--;
-                } else {
-                    bracketsArray[i] = ')';
-                }
-            }
+            bracketsArray[indexOfOpenedBracketsToChange] = ')';
+            //проходимся по оставшимся скобкам справа от измененной и меняем их.
+            changeRemainingBracketsOnTheRight(bracketsNumber, bracketsArray, indexOfOpenedBracketsToChange);
             writeLine(bracketsArray);
         }
+    }
 
+    private static int findIndexOfOpenedBracketToChange(int bracketsNumber, char[] bracketsArray) {
+        int countOfClosedBrackets = 0;
+        for (int i = bracketsNumber - 1 ; i >= 0 ; i--) {//ищем с конца последовательности, чтобы поменять самую правую
+             if (bracketsArray[i] == ')') {              //открывающуюся скобку'(' на закрывающую ')', т.к. '(' < ')'
+                countOfClosedBrackets++;                 //таким образом мы формируем последовательность, которая будет больше.
+            }
+            if (bracketsArray[i] == '(') {
+                countOfOpenedBrackets++;//важно запомнить, сколько открывающих скобок было в пройденной части
+            }
+            //если мы встретили открывающую скобку и закрывающих скобок больше, чем открывающих, то это открывающая скобка, которую можно поменять на закрывающую
+            if (countOfOpenedBrackets > 0 && bracketsArray[i] == '(' && countOfOpenedBrackets - countOfClosedBrackets < 0) {
+                return i;//возвращаем индекс этой скобки
+            }
+        }
+        return -1;
+    }
+
+    private static void changeRemainingBracketsOnTheRight(int bracketsNumber, char[] bracketsArray, int indexOfOpenedBracketsToChange) {
+        for (int i = indexOfOpenedBracketsToChange + 1; i < bracketsNumber; i++) {
+            if (countOfOpenedBrackets > 0) {
+                //сначала меняем на отрывающие скобки. Столько, сколько было открывающих скобок при поиске индекса.
+                bracketsArray[i] = '(';
+                countOfOpenedBrackets--;
+                continue;
+            }
+            //оставшиеся меняем на закрывающие
+            bracketsArray[i] = ')';
+            //таким образом мы формируем самую маленькую скобочную последовательность, посколько '(' < ')'.
+        }
     }
 
     private static void writeLine(char[] bracketsArray) throws IOException {
