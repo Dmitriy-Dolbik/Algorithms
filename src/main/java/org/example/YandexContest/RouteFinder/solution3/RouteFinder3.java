80fa0fa6e041f87aabf8adf42322a4ff6f1c675c
ddolbik <d.dolbik@crystals.ru>
2024-02-16 11:08:53 +0300

    Solution in One class

--- src/main/java/org/example/YandexContest/RouteFinder/solution3/RouteFinder3.java
+++ src/main/java/org/example/YandexContest/RouteFinder/solution3/RouteFinder3.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  * Источник
@@ -57,5 +58,117 @@ public class RouteFinder3 {
     private static void close() throws IOException {
         bufferedWriter.close();
         bufferedReader.close();
+    }
+}
+
+class Graph {
+    private final int maxCountOfCities = 1000;
+    private int[][] adjacencyMatrix;
+    private City[] citiesList;
+    private int countOfCities;
+    private final LinkedList<Integer> queue = new LinkedList<>();
+
+    public Graph() {
+        this.citiesList = new City[maxCountOfCities];
+        this.adjacencyMatrix = new int[maxCountOfCities][maxCountOfCities];
+        this.countOfCities = 0;
+    }
+
+    public void addCity(City city) {
+        citiesList[countOfCities] = city;
+        countOfCities++;
+    }
+
+    public int getNotVisitedCityFor(int fromCityIndex, int maxDistance) {
+        for (int i = 0; i < countOfCities; i++) {
+            if (getDistanceBetween(fromCityIndex, i) <= maxDistance && !citiesList[i].isVisited()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int getDistanceBetween(int fromCityIndex, int i) {
+        City fromCity = citiesList[fromCityIndex];
+        City iterratedCity = citiesList[i];
+        return Math.abs(fromCity.getX() - iterratedCity.getX()) + Math.abs(fromCity.getY() - iterratedCity.getY());
+
+    }
+
+    public void passInDeep(int fromCityIndex, int toCityIndex, int maxDistance) {
+        City fromCity = citiesList[fromCityIndex];
+        fromCity.setVisited(true);
+        queue.add(fromCityIndex);
+
+        while(!queue.isEmpty()) {
+            int nextCity = getNotVisitedCityFor(queue.getLast(), maxDistance);
+
+            if  (nextCity == -1) {
+                nextCity = queue.removeLast();
+            } else {
+                citiesList[nextCity].setVisited(true);
+                queue.add(nextCity);
+            }
+        }
+    }
+
+    public int passInWidth(int fromCityIndex, int toCityIndex, int maxDistance) {
+        int indexOfFromCityInGraf = fromCityIndex - 1;
+        int indexOfToCityInGraf = toCityIndex - 1;
+        LinkedList<int[]> queue = new LinkedList<>();
+
+        citiesList[indexOfFromCityInGraf].setVisited(true);
+        queue.add(new int[]{indexOfFromCityInGraf, 0});
+
+        while (!queue.isEmpty()) {
+            int[] cityIndexAndCountOfRoadsToIt = queue.removeFirst();
+            int currentIndex = cityIndexAndCountOfRoadsToIt[0];
+            int countOfRoads = cityIndexAndCountOfRoadsToIt[1];
+
+            if (currentIndex == indexOfToCityInGraf) {
+                return countOfRoads;
+            }
+
+            while(true) {
+                int nextCityIndex = getNotVisitedCityFor(currentIndex, maxDistance);
+
+                if (nextCityIndex == -1) {
+                    break;
+                }
+
+                citiesList[nextCityIndex].setVisited(true);
+                queue.add(new int[]{nextCityIndex, countOfRoads + 1});
+            }
+        }
+
+        return -1;
+    }
+}
+
+class City {
+    private boolean isVisited;
+    private final int x;
+    private final int y;
+
+    public City(int x, int y) {
+        this.isVisited = false;
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isVisited() {
+        return isVisited;
+    }
+
+    public void setVisited(boolean visited) {
+        isVisited = visited;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
