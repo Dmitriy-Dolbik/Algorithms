206f78049a81d92e721ff9a40e09efb9d3683773
ddolbik <d.dolbik@crystals.ru>
2024-02-27 21:53:50 +0300

    Add ValidParentheses solution

--- src/main/java/org/example/leetcode/Stack/ValidParentheses.java
+++ src/main/java/org/example/leetcode/Stack/ValidParentheses.java
@@ -2,9 +2,47 @@ package org.example.leetcode.Stack;
 
 import org.example.leetcode.Task;
 
-public class ValidParentheses implements Task {
-    public boolean isValid(String s) {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
+public class ValidParentheses implements Task {
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> closedAndOpenedBrackets = new HashMap<>();
+        closedAndOpenedBrackets.put(')', '(');
+        closedAndOpenedBrackets.put(']', '[');
+        closedAndOpenedBrackets.put('}', '{');
+
+        char[] charArray = s.toCharArray();
+        for (char character : charArray) {
+            //Если открывающая скобка, то кладем её в стек
+            if (isOpenedBracket(character, closedAndOpenedBrackets)) {
+                stack.add(character);
+            } else {
+                //Если это закрывающая скобка, то в стеке должна лежать открывающая
+                if (stack.empty()) {
+                    return false;
+                }
+                //Если закрывающая скобка, то достаем открывающую скобку из стека
+                int openParentheses = stack.pop();
+                //Закрывающая скобка должна идти после открывающей того же типа
+                char matchedOpenedBracket = closedAndOpenedBrackets.get(character);
+                if (openParentheses != matchedOpenedBracket) {
+                    return false;
+                }
+            }
+        }
+        //Если в стеке что-то осталось, то открывающих скобок больше, чем закрывающих
+        return stack.empty();
+    }
+
+    private static boolean isOpenedBracket(char character, Map<Character, Character> closedAndOpenedBrackets) {
+        return !closedAndOpenedBrackets.containsKey(character);
     }
 
     @Override
@@ -14,22 +52,79 @@ public class ValidParentheses implements Task {
 
     @Override
     public void getMySolution() {
-
+//        Stack<Character> stack = new Stack<>();
+//        char[] charArray = s.toCharArray();
+//        for (char character : charArray) {
+//            //Если открывающая скобка, то кладем её в стек
+//            if (character == '(' || character == '[' || character == '{') {
+//                stack.add(character);
+//            } else {
+//                //Если это закрывающая скобка, то в стеке должна лежать открывающая
+//                if (stack.empty()) {
+//                    return false;
+//                }
+//                //Если закрывающая скобка, то достаем открывающую скобку из стека
+//                int openParentheses = stack.pop();
+//                //Закрывающая скобка должна идти после открывающей того же типа
+//                if (openParentheses == '(' && character != ')'
+//                        ||
+//                        openParentheses == '[' && character != ']'
+//                        ||
+//                        openParentheses == '{' && character != '}') {
+//                    return false;
+//                }
+//            }
+//        }
+//        //Если в стеке что-то осталось, то открывающих скобок больше, чем закрывающих
+//        if (!stack.empty()) {
+//            return false;
+//        }
+//        return true;
     }
 
     @Override
     public void getOptimizeSolution() {
-
+        //Решение такое же, как мое, только через Map
+//        Stack<Character> stack = new Stack<>();
+//        Map<Character, Character> closedAndOpenedBrackets = new HashMap<>();
+//        closedAndOpenedBrackets.put(')', '(');
+//        closedAndOpenedBrackets.put(']', '[');
+//        closedAndOpenedBrackets.put('}', '{');
+//
+//        char[] charArray = s.toCharArray();
+//        for (char character : charArray) {
+//            //Если открывающая скобка, то кладем её в стек
+//            if (isOpenedBracket(character, closedAndOpenedBrackets)) {
+//                stack.add(character);
+//            } else {
+//                //Если это закрывающая скобка, то в стеке должна лежать открывающая
+//                if (stack.empty()) {
+//                    return false;
+//                }
+//                //Если закрывающая скобка, то достаем открывающую скобку из стека
+//                int openParentheses = stack.pop();
+//                //Закрывающая скобка должна идти после открывающей того же типа
+//                char matchedOpenedBracket = closedAndOpenedBrackets.get(character);
+//                if (openParentheses != matchedOpenedBracket) {
+//                    return false;
+//                }
+//            }
+//        }
+//        //Если в стеке что-то осталось, то открывающих скобок больше, чем закрывающих
+//        if (!stack.empty()) {
+//            return false;
+//        }
+//        return true;
     }
 
     @Override
     public String getTimeComplexity() {
-        return null;
+        return "O(n)";
     }
 
     @Override
     public String getSpaceComplexity() {
-        return null;
+        return "O(n)";
     }
 }
 
